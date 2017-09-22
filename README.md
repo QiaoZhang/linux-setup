0e55a05adda51cc704f46637a091aed64c94e658
Qiao Zhang <qiaoz@smu.edu>
2017-09-21 19:17:28 -0500

    add MySQL Workbench and update README.md

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Script for ubuntu silient setup
+
+1. Install JDK with Java 8
+2. Install IntelliJ
+3. Install MySQL with root_password "root"
+    3a. Install MySQL Workbench
+4. Install Python-pip
+5. Install Apache Tomcat 8 *(Not configured yet)
--- dev-env.sh
+++ dev-env.sh
@@ -16,9 +16,11 @@
 #echo 'Install IntelliJ'
 #apt-get install -y -q intellij-idea-community
 
+#echo 'Install MySQL and MySQL Workbench'
 #echo 'mysql-server-5.7 mysql-server/root_password password root' | debconf-set-selections
 #echo 'mysql-server-5.7 mysql-server/root_password_again password root' | debconf-set-selections
-#apt-get -y install mysql-server-5.7
+#apt-get -y -q install mysql-server-5.7
+#apt-get -y -q install mysql-workbench
 
 #echo 'Install Python-pip'
 #apt-get install -y -q python-pip
