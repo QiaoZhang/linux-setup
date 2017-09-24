a2a9fceba106e5ba3bcb1dd4bd035747cffd7e40
Qiao Zhang <qiaoz@smu.edu>
2017-09-24 18:56:33 -0500

    Add installation of NodeJS, NPM, and Instant-Markdown-Preview

--- dev-env.sh
+++ dev-env.sh
@@ -19,8 +19,8 @@
 #echo 'Install MySQL and MySQL Workbench'
 #echo 'mysql-server-5.7 mysql-server/root_password password root' | debconf-set-selections
 #echo 'mysql-server-5.7 mysql-server/root_password_again password root' | debconf-set-selections
-#apt-get -y -q install mysql-server-5.7
-#apt-get -y -q install mysql-workbench
+#apt-get install -y -q  mysql-server-5.7
+#apt-get install -y -q  mysql-workbench
 
 #echo 'Install Python-pip'
 #apt-get install -y -q python-pip
@@ -29,3 +29,11 @@
 #apt-get install -y -q tomcat8-admin
 #apt-get install -y -q tomcat8
 
+#echo 'Install NodeJS and NPM'
+#apt-get install -y -q nodejs
+#apt-get install -y -q npm
+
+#echo 'Install Instant MarkdownPreview'
+#apt-get install -y -q xdg-utils
+#apt-get install -y -q build-essential
+#npm -g install instant-markdown-d
